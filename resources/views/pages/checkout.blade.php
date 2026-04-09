bf4ae5fbe6ffd59eb8b95030b8bec6707e9c6a5b
hv-0519 <hetp967@gmail.com>
2026-04-09 09:43:26 +0530

    fixed the checkout

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -363,7 +363,7 @@
                         <div class="summary-item-row">
                             <div class="summary-item-img">
                                 @if($item['image'])
-                                    <img src="{{ ['image'] }}" alt="{{ $item['name'] }}">
+                                    <img src="{{ $item['image'] }}" alt="{{ $item['name'] }}">
                                 @else
                                     {{ $icons[$item['category']] ?? '🍽️' }}
                                 @endif
